4ad6eec4ebba10a906f7379e6a55a580f339ee02
Marcos José Moura Pinho Junior <marcos.pinhojr@outlook.com>
2019-12-14 16:34:31 -0300

    creating new homepage and fixing header style. Plus, creating new route class to separate template generator routes from request routes

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import app
 from app.controllers.main import Main
+from app.controllers.main import MainReqs
 from app.controllers.general_analysis import GeneralAnalysis
 
 
 class Route():
     def __init__(self):
         Main()
+        MainReqs()
         GeneralAnalysis()
