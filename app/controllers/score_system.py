fc18a3985031cef64a7c759440bb1142bf4267ac
MarcosJM <marcos.pinhojr@outlook.com>
2020-03-02 13:40:45 -0300

    adding finalscore to ranking list

--- app/controllers/score_system.py
+++ app/controllers/score_system.py
@@ -431,17 +431,21 @@ class ScoreSystem:
                 allDeputiesIds = utils.getAllDeputiesIds()
 
                 for _, deputy_id in enumerate(allDeputiesIds):
-                    if utils.isEligible(deputy_id):
-                        print("*******D E P U T Y     I D", deputy_id, " - ", _, " O F", len(allDeputiesIds), '*******')
-                        indicator_one_score = calculateIndicatorOneScore(deputy_id)
-                        indicator_two_score = calculateIndicatorTwoScore(deputy_id)
-                        indicator_three_score = calculateIndicatorThreeScore(deputy_id)
+                    # if utils.isEligible(deputy_id):
+                    print("*******D E P U T Y     I D", deputy_id, " - ", _, " O F", len(allDeputiesIds), '*******')
+                    indicator_one_score = calculateIndicatorOneScore(deputy_id)['indicator_one_score']
+                    indicator_two_score = calculateIndicatorTwoScore(deputy_id)
+                    indicator_three_score = calculateIndicatorThreeScore(deputy_id)
+                    finalScore = (indicator_one_score + indicator_two_score + indicator_three_score) / 3
 
-                        scores = {'scores': {'indicator_one': indicator_one_score,
-                                             'indicator_two': indicator_two_score,
-                                             'indicator_three': indicator_three_score}}
+                    scores = {'scores': {'indicator_one': indicator_one_score,
+                                         'indicator_two': indicator_two_score,
+                                         'indicator_three': indicator_three_score,
+                                         'final': finalScore}}
 
-                        allScores[deputy_id] = scores
+                    # it needs to be sorted!!
+                    allScores[deputy_id] = scores
+
 
                     # write data into a JSON file
                     utils.dict_to_json_file(allScores, utils.RANKING_FILE_DIRECTORY, 'indicadors.json')
